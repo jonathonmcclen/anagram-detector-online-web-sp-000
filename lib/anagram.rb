43d53c4e5e6f9e9b99bcb2f621e29d4d0787f08a
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-30 15:58:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :somthing
+  
+  def initialize(word) 
+    @word = word
+  end 
+  
+  def match(array)
+    puts array
+    array.detect {|set| set == @word}
+  end 
+end 
